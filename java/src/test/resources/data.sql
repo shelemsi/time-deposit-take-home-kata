f3748002b748e003606ae84e13c43e3fd7b29440
Siya <30735455+shelemsi@users.noreply.github.com>
2026-02-02 03:01:43 +0200

    chore: add mock data for API testing. On start up, you can move data.sql file to resources of main.

---
+++ java/src/test/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO TIME_DEPOSITS (ID, PLAN_TYPE, BALANCE, DAYS)
+VALUES
+    (1, 'Basic',   1000.0000, 31),
+    (2, 'Student', 1000.0000, 340),
+    (3, 'Premium', 1000.0000, 40),
+    (4, 'Basic',   1500.0000, 10);
